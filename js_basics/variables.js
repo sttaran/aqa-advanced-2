1314bd696a6209ff8df92758ced5e9bce3c09ef4
staran <staran@griddynamics.com>
2023-10-03 21:21:22 +0300

    staran // add js basics lesson code - part 1

---
+++ js_basics/variables.js
@@ -0,0 +1,40 @@
+
+// VARIABLES
+
+// const age = 18
+// age = 12
+// let name = "Bob"
+// console.log(name)
+// name = "Joe"
+// console.log(name)
+// var topic = "JS Basics"
+// topic = "ololo"
+// console.log(topic)
+
+
+// YOUR CODE BEFORE
+// console.log(name)
+//
+// var name = "Joe"
+//
+// // AFTER
+// var name
+//
+// console.log(name)
+//
+// name = "Joe"
+
+
+// const age = 12;
+// let age2;
+//
+// age2 = 222
+
+// NAMING
+// CAMEL CASE
+const personAge = 23
+const streetName = "Central";
+const ADULT_AGE = 18
+
+// SNAKE CASE
+// const person_age = 23
