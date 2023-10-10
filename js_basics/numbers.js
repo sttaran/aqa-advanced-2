8f6ccf5ea7a6fc509afdb9d63f10fe55e892b264
staran <staran@griddynamics.com>
2023-10-10 21:27:36 +0300

    staran // add js basics lesson code - part 2

---
+++ js_basics/numbers.js
@@ -0,0 +1,26 @@
+// const ageString = '18'
+// // console.log(ageString)
+// // console.log(typeof ageString)
+//
+// // const age = Number(ageString)
+// const age = +ageString
+// console.log(age)
+// console.log(typeof age)
+
+// const stringValue = "w123.4px"
+// console.log(Number(stringValue))
+// console.log(typeof Number(stringValue))
+// const pxValue = Number.parseInt(stringValue)
+// const pxValue = Number.parseFloat(stringValue)
+// console.log(pxValue)
+// console.log(typeof pxValue)
+
+// const result  = 3 / 1
+// console.log(isNaN(result))
+
+// const x = 0.1;
+// const y = 0.2;
+// const sum = x + y;
+// console.log(+sum.toFixed(2))
+
+console.log(Math.floor(Math.random() * 100))
---
+++ js_basics/operators.js
@@ -0,0 +1,23 @@
+// console.log(3 > 4)
+// console.log(3 < 4)
+
+// console.log(3 >= 4)
+// console.log(4 <= 4)
+
+// console.log(3 == 3)
+// console.log(3 != 3)
+// console.log(3 != 4)
+
+// console.log(3 === 3)
+// console.log(3 !== 3)
+// console.log(3 !== 4)
+
+// console.log(3 == '3')
+// console.log(3 === '3')
+
+// console.log( 1 == false)
+
+//---------------------------------
+
+const sum = 9 % 2
+console.log(sum)
